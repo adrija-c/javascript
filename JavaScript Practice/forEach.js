fa4a2c340e311091ed2c39b67f846e77a8784886
adrija-c <146987368+adrija-c@users.noreply.github.com>
2023-10-13 01:15:42 +0530

    Update forEach.js

--- JavaScript Practice/forEach.js
+++ JavaScript Practice/forEach.js
@@ -1,30 +1,24 @@
-function forEachPrime(number) {
-    
-    if (number < 2) 
-      return false;
-    
-    for (let i = 2; i < number; i++) {
-        if (number % i === 0) 
-          return false;
+function forEach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i, arr);
+    }
+}
+
+function isPrime(num) {
+    if (num < 2) 
+        return false;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) 
+            return false;
     }
     return true;
 }
 
-function printPrimeNumbers(max) {
-    const primes = [];
-    for (let i = 2; i <= max; i++) {
-        if (forEachPrime(i)) {
-            primes.push(i);
-        }
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+forEach(numbers, function(element) {
+    if (isPrime(element)) {
+        console.log(element);
     }
-    return primes;
-}
-
-const maxNumber = 30; 
-const primeNumbers = printPrimeNumbers(maxNumber);
-
-primeNumbers.forEach(function(prime) {
-    
-    console.log(prime);
 });
 
