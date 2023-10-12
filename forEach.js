721e8fa2ea76c6a77b891173e33edcc7699206ba
adrija-c <146987368+adrija-c@users.noreply.github.com>
2023-10-13 01:36:17 +0530

    Add files via upload

---
+++ forEach.js
@@ -0,0 +1,23 @@
+function forEach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i, arr);
+    }
+}
+
+function isPrime(num) {
+    if (num < 2) 
+        return false;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) 
+            return false;
+    }
+    return true;
+}
+
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+forEach(numbers, function(element) {
+    if (isPrime(element)) {
+        console.log(element);
+    }
+});
---
+++ map.js
@@ -0,0 +1,17 @@
+function mapArray(arr, callbackFunc) {
+    const finalList = [];
+    for (let i = 0; i < arr.length; i++) {
+        const val = callbackFunc(arr[i]);
+        finalList.push(val);
+    }
+    return finalList;
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const squaredNumbers = mapArray(numbers, function(num) {
+    return num * num;
+});
+
+
+console.log("Square of the numbers : 1, 2, 3, 4, 5, 6, 7 are ", squaredNumbers);
