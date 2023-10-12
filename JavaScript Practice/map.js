1777267902f4e705d042272c05dc6760d378fde8
adrija-c <146987368+adrija-c@users.noreply.github.com>
2023-10-13 01:25:51 +0530

    Update map.js

--- JavaScript Practice/map.js
+++ JavaScript Practice/map.js
@@ -1,7 +1,8 @@
-function mapArray(arr, transformation) {
+ffunction mapArray(arr, callbackFunc) {
     const finalList = [];
     for (let i = 0; i < arr.length; i++) {
-        finalList.push(transformation(arr[i]));
+        const val = callbackFunc(arr[i]);
+        finalList.push(val);
     }
     return finalList;
 }
@@ -12,5 +13,6 @@ const squaredNumbers = mapArray(numbers, function(num) {
     return num * num;
 });
 
-console.log('Square of the numbers : 1, 2, 3, 4, 5, 6, 7 are ');
-console.log(squaredNumbers);
+
+console.log("Square of the numbers : 1, 2, 3, 4, 5, 6, 7 are ", squaredNumbers);
+
